bf5a370c9b4ffeb3c008b99cae79f6399bc239f0
Dawnlich <63298353+Dawnlich@users.noreply.github.com>
2021-12-13 13:23:07 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html> 
+<html>
+<head>
+    <title> LOGIN PAGE </title>
+    <link rel="stylesheet" type="text/css" href="login.css">
+</head>
+<body>
+    <form action ="login.php" method="post">
+        <h2>Welcome to OSTORE!</h2>
+        <h3>User's Name:</h3>
+        <input type ="text" name="uname"  placeholder="Please enter username" required><br>
+        <h3>Password:</h3>
+        <input type ="password" name="password"  placeholder="Please enter passowrd" required><br>
+        <button type="submit">Sign in</button>
+    </form>
+</body>
+</html>
