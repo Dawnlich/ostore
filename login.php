68d4ae4ab816d48dd6d20e7a620e7825771f6eec
Dawnlich <63298353+Dawnlich@users.noreply.github.com>
2021-12-14 09:41:43 -0500

    Add files via upload

---
+++ login.php
@@ -0,0 +1,26 @@
+<?php 
+session_start(); 
+include "database.php";
+if (isset($_POST['uname']) && isset($_POST['password'])) {
+    $uname = $_POST['uname'];
+    $pass = $_POST['password'];
+    $sql = "SELECT * FROM accounts WHERE username='$uname' AND password='$pass'";
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($result) === 1) {
+        $row = mysqli_fetch_assoc($result);
+        if ($row['username'] === $uname && $row['password'] === $pass) {
+            $_SESSION['company_name'] = $row['company_name'];
+            $_SESSION['name'] = $row['name'];
+            $_SESSION['username'] = $row['username'];
+            $_SESSION['id'] = $row['id'];
+            header("Location: home.php");
+            exit();
+        }
+    }else{
+        header("Location: index.php?");
+        exit();
+    }
+}else{
+    header("Location: index.php");
+    exit();
+}
